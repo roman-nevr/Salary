ddd9fe1994fd21ee83efd5307aad1c0bb9b20255
roman-nevr <roman-nevr@yandex.ru>
2016-11-27 22:55:08 +0300

    demo

--- app/src/main/java/ru/rubicon/salary/NavigationScreen.java
+++ app/src/main/java/ru/rubicon/salary/NavigationScreen.java
@@ -13,6 +13,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
@@ -63,6 +64,7 @@ public class NavigationScreen extends AppCompatActivity implements SalaryListFra
         actionButton = (FloatingActionButton) findViewById(R.id.fab);
         clMainList = (CoordinatorLayout) findViewById(R.id.clMainList);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         fragmentManager = getSupportFragmentManager();
         fragmentManager.addOnBackStackChangedListener(new BackStackChangedListener());
@@ -90,6 +92,13 @@ public class NavigationScreen extends AppCompatActivity implements SalaryListFra
         bundle.putInt(DatabaseHelper._ID, position);
         calcFragment.setArguments(bundle);
         showNewFragmentWithBackStack(calcFragment, TAG_4);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
 
--- app/src/main/java/ru/rubicon/salary/fragments/SalaryListFragment.java
+++ app/src/main/java/ru/rubicon/salary/fragments/SalaryListFragment.java
@@ -3,7 +3,10 @@ package ru.rubicon.salary.fragments;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.BaseAdapter;
 import android.widget.ListAdapter;
@@ -15,6 +18,7 @@ import java.util.Arrays;
 import ru.rubicon.salary.DAO.DatabaseHelper;
 import ru.rubicon.salary.DAO.EmployeeDataSource;
 import ru.rubicon.salary.DAO.SalaryDataSource;
+import ru.rubicon.salary.R;
 import ru.rubicon.salary.adapter.EmployeeSalariesListAdapter;
 import ru.rubicon.salary.adapter.MainActivityAdapter;
 import ru.rubicon.salary.entity.Employee;
@@ -58,52 +62,14 @@ public class SalaryListFragment extends ListFragment {
 
         setRetainInstance(true);
 
+        setHasOptionsMenu(true);
+
     }
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
         mCallback.onSalaryItemClick(salaries.get(position).getId());
-        /*dataSource.openForRead();
-        employeeDataSource.openForRead();
-        employees = employeeDataSource.readAllEmployees();
-        Salary salary = dataSource.readSalary(salaries.get(position).getId(), employees);*/
-
-        /*CalcTemporalFragmentAlt calcFragment = new CalcTemporalFragmentAlt();
-
-        Bundle bundle = new Bundle();
-        bundle.putInt(DatabaseHelper._ID, salaries.get(position).getId());
-        calcFragment.setArguments(bundle);
-        calcFragment.setTargetFragment(this, position);*/
-
-        /*
-        Employee employee = employees.get(position);
-        int employeeId = employee.getId();
-        EmployeeDetailsDialogFragment detailsFragment = new EmployeeDetailsDialogFragment();
-
-        Bundle bundle = new Bundle();
-        bundle.putInt(DIALOG_ID, employeeId);
-        bundle.putString(DIALOG_NAME, employee.getName());
-        bundle.putFloat(DIALOG_COEF, employee.getCoefficient());
-        bundle.putString(DIALOG_COMMENT, employee.getComment());
-        bundle.putBoolean(DIALOG_ACTIVE, employee.isActive());
-
-        detailsFragment.setArguments(bundle);
-        detailsFragment.setTargetFragment(this, position);
-        detailsFragment.show(getFragmentManager(), "ask_details");
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode == Activity.RESULT_OK){
-            dataSource.openForRead();
-            employees = dataSource.readAllEmployees();
-            dataSource.close();
-            adapter.notifyDataSetInvalidated();
-        }
-    }
-         */
     }
 
     @Override
@@ -117,5 +83,11 @@ public class SalaryListFragment extends ListFragment {
 
         adapter = new EmployeeSalariesListAdapter(getContext(), salaries);
         setListAdapter(adapter);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_main, menu);
     }
 }
